fb02bc77a541bc3bab371b261f6bcc8ea1c54cf0
Haotian <hubery.luo@mail.utoronto.ca>
2020-02-04 15:02:15 -0500

    starting plots for part 2

--- a1/Part2.py
+++ a1/Part2.py
@@ -3,15 +3,14 @@ import numpy as np
 import starter
 
 def print_info(loss, train_a, valid_a, test_a, type, alpha, reg, comp_time):    
-    if type is "GD":
+    if type is "MSE":
         print('Batch GD with \u03B1 = {}, \u03BB = {}, MSE = {}, training accuracy = {}, valid accuracy = {}, test accuracy = {}, '
             'computation time = {} ms'.format(alpha, reg, loss, train_a, valid_a, test_a, int(comp_time * 1000)))
-    elif type is "normal":
-        print('Normal Equation with \u03BB = {}, MSE = {}, training accuracy = {}, valid accuracy = {}, test accuracy = {}, '
-            'computation time = {} ms'.format(reg, loss, train_a, valid_a, test_a, int(comp_time * 1000)))
+    elif type is "CE":
+        print('Batch GD with \u03BB = {}, \u03BB = {}, CE = {}, training accuracy = {}, valid accuracy = {}, test accuracy = {}, '
+            'computation time = {} ms'.format(alpha, reg, loss, train_a, valid_a, test_a, int(comp_time * 1000)))
 
 def linreg():
-    #1.3
 
     import time
 
@@ -21,45 +20,35 @@ def linreg():
     testData = np.array([x.flatten() for x in testData])
 
 
-    alpha1, alpha2, alpha3 = 0.005, 0.001, 0.0001
-    reg = 0
+    #2.2
+
+    alpha = 0.005
+    reg = 0.1
     epochs = 5000
     error_tol = 1e-7
 
-    init_weight = np.random.normal(size=(784,1))
-    init_bias = np.random.uniform(-1,1)
+    init_weight = np.random.normal(0, 0.5, size=(784,1))
+    init_bias = 0
 
     start1 = time.time()
-    weight_train1, bias_train1, loss_train1 = grad_descent(init_weight, init_bias, trainData, trainTarget, alpha1, epochs, reg, error_tol)
+    weight_train1, bias_train1, loss_train1 = grad_descent(init_weight, init_bias, trainData, trainTarget, alpha1, epochs, reg, error_tol, "CE")
     end1 = time.time()
-    start2 = time.time()
-    weight_train2, bias_train2, loss_train2 = grad_descent(init_weight, init_bias, trainData, trainTarget, alpha2, epochs, reg, error_tol)
-    end2 = time.time()
-    start3 = time.time()
-    weight_train3, bias_train3, loss_train3 = grad_descent(init_weight, init_bias, trainData, trainTarget, alpha3, epochs, reg, error_tol)
-    end3 = time.time()
-
+    
+    loss_valid1 = crossEntropyLoss(weight_train1, bias_train1, validData, validTarget, reg)
+    loss_test1 = crossEntropyLoss(weight_train1, bias_train1, testData, testTarget, reg)
 
     accuracy_train1 = accuracy_calculation(weight_train1, bias_train1, trainData, trainTarget)
-    accuracy_train2 = accuracy_calculation(weight_train2, bias_train2, trainData, trainTarget)
-    accuracy_train3 = accuracy_calculation(weight_train3, bias_train3, trainData, trainTarget)
-
     accuracy_valid1 = accuracy_calculation(weight_train1, bias_train1, validData, validTarget)
-    accuracy_valid2 = accuracy_calculation(weight_train2, bias_train2, validData, validTarget)
-    accuracy_valid3 = accuracy_calculation(weight_train3, bias_train3, validData, validTarget)
-
     accuracy_test1 = accuracy_calculation(weight_train1, bias_train1, testData, testTarget)
-    accuracy_test2 = accuracy_calculation(weight_train2, bias_train2, testData, testTarget)
-    accuracy_test3 = accuracy_calculation(weight_train3, bias_train3, testData, testTarget)
 
     plt.figure()
-    plt.suptitle('Training losses')
-    plt.plot(loss_train1,'',loss_train2,'',loss_train3,'')
+    plt.suptitle('Cross Entropy loss curves')
+    plt.plot(loss_train1,'',loss_valid1,'',loss_test1,'')
     plt.xlabel('epochs')
     plt.ylabel('losses')
     plt.grid()
-    plt.legend(['MSE: \u03B1 = {}, \u03BB = {}'.format(alpha1, reg),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha2, reg),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha3, reg)])
-    plt.savefig('Learning_rate_adjustment_training_loss_LinReg.png')
+    plt.legend(['Training Data Loss', 'Validation Data Loss', 'Test Data Loss'])
+    plt.savefig('Cross_Entropy_loss_LogReg.png')
 
 
     plt.figure()
@@ -68,52 +57,29 @@ def linreg():
     plt.xlabel('epochs')
     plt.ylabel('accuracy')
     plt.grid()
-    plt.legend(['MSE: \u03B1 = {}, \u03BB = {}'.format(alpha1, reg),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha2, reg),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha3, reg)])
-    plt.savefig('Learning_rate_adjustment_training_accuracy_LinReg.png')
+    plt.legend(['Training Accuracy', 'Validation Accuray', 'Test Accuracy'])
+    plt.savefig('Cross_Entropy_accuracy_LogReg.png')
     
 
-    plt.figure()
-    plt.suptitle('Validation accuracy')
-    plt.plot(accuracy_valid1,'',accuracy_valid2,'',accuracy_valid3,'')
-    plt.xlabel('epochs')
-    plt.ylabel('accuracy')
-    plt.grid()
-    plt.legend(['MSE: \u03B1 = {}, \u03BB = {}'.format(alpha1, reg),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha2, reg),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha3, reg)])
-    plt.savefig('Learning_rate_adjustment_validation_accuracy_LinReg.png')
-    
-
-    plt.figure()
-    plt.suptitle('Testing accuracy')
-    plt.plot(accuracy_test1,'',accuracy_test2,'',accuracy_test3,'')
-    plt.xlabel('epochs')
-    plt.ylabel('accuracy')
-    plt.grid()
-    plt.legend(['MSE: \u03B1 = {}, \u03BB = {}'.format(alpha1, reg),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha2, reg),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha3, reg)])
-    plt.savefig('Learning_rate_adjustment_testing_accuracy_LinReg.png')
-
-    print_info(loss_train1[-1], accuracy_train1[-1], accuracy_valid1[-1], accuracy_test1[-1], "GD", alpha1, reg, end1 - start1)
-    print_info(loss_train2[-1], accuracy_train2[-1], accuracy_valid2[-1], accuracy_test2[-1], "GD", alpha2, reg, end2 - start2)
-    print_info(loss_train3[-1], accuracy_train3[-1], accuracy_valid3[-1], accuracy_test3[-1], "GD", alpha3, reg, end3 - start3)
+    print_info(loss_train1[-1], accuracy_train1[-1], accuracy_valid1[-1], accuracy_test1[-1], "CE", alpha1, reg, end1 - start1)
+    print_info(loss_train2[-1], accuracy_train2[-1], accuracy_valid2[-1], accuracy_test2[-1], "CE", alpha2, reg, end2 - start2)
+    print_info(loss_train3[-1], accuracy_train3[-1], accuracy_valid3[-1], accuracy_test3[-1], "CE", alpha3, reg, end3 - start3)
 
 
-    #1.4
-    alpha = 0.005
-    reg1, reg2, reg3 = 0.001, 0.1, 0.5
+    #2.3
+
+    reg = 0
     
     start4 = time.time()
-    weight_train4, bias_train4, loss_train4 = grad_descent(init_weight, init_bias, trainData, trainTarget, alpha, epochs, reg1, error_tol)
+    weight_train2, bias_train2, loss_train2 = grad_descent(init_weight, init_bias, trainData, trainTarget, alpha, epochs, reg1, error_tol)
     end4 = time.time()
-    start5 = time.time()
-    weight_train5, bias_train5, loss_train5 = grad_descent(init_weight, init_bias, trainData, trainTarget, alpha, epochs, reg2, error_tol)
-    end5 = time.time()
-    start6 = time.time()
-    weight_train6, bias_train6, loss_train6 = grad_descent(init_weight, init_bias, trainData, trainTarget, alpha, epochs, reg3, error_tol)
-    end6 = time.time()
 
+    loss_valid2 = crossEntropyLoss(weight_train2, bias_train2, validData, validTarget, reg)
+    loss_test2 = crossEntropyLoss(weight_train2, bias_train2, testData, testTarget, reg)
 
-    accuracy_train4 = accuracy_calculation(weight_train4, bias_train4, trainData, trainTarget)
-    accuracy_train5 = accuracy_calculation(weight_train5, bias_train5, trainData, trainTarget)
-    accuracy_train6 = accuracy_calculation(weight_train6, bias_train6, trainData, trainTarget)
+    accuracy_train2 = accuracy_calculation(weight_train2, bias_train2, trainData, trainTarget)
+    accuracy_valid2 = accuracy_calculation(weight_train2, bias_train2, validData, validTarget)
+    accuracy_test2 = accuracy_calculation(weight_train2, bias_train2, testData, testTarget)
 
     plt.figure()
     plt.suptitle('Training losses')
@@ -134,9 +100,6 @@ def linreg():
     plt.legend(['MSE: \u03B1 = {}, \u03BB = {}'.format(alpha, reg1),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha, reg2),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha, reg3)])
     plt.savefig('Regulation_adjustment_training_accuracy_LinReg.png')
 
-    accuracy_valid4 = accuracy_calculation(weight_train4, bias_train4, validData, validTarget)
-    accuracy_valid5 = accuracy_calculation(weight_train5, bias_train5, validData, validTarget)
-    accuracy_valid6 = accuracy_calculation(weight_train6, bias_train6, validData, validTarget)
 
     plt.figure()
     plt.suptitle('Validation accuracy')
@@ -147,9 +110,6 @@ def linreg():
     plt.legend(['MSE: \u03B1 = {}, \u03BB = {}'.format(alpha, reg1),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha, reg2),'MSE: \u03B1 = {}, \u03BB = {}'.format(alpha, reg3)])
     plt.savefig('Regulation_adjustment_validation_accuracy_LinReg.png')
 
-    accuracy_test4 = accuracy_calculation(weight_train4, bias_train4, testData, testTarget)
-    accuracy_test5 = accuracy_calculation(weight_train5, bias_train5, testData, testTarget)
-    accuracy_test6 = accuracy_calculation(weight_train6, bias_train6, testData, testTarget)
 
     plt.figure()
     plt.suptitle('Testing accuracy')
